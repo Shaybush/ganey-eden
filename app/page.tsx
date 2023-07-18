883bc3aa928b3b4ef0c3e2b9c918c8af0a5d59cd
Shay Bushary <shay.bushary@1touch.io>
2023-07-19 02:32:44 +0300

    icon example done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Button from './shared/components/button/button';
 import Icon from './shared/components/icon/icon';
-import Img from './assets/icons/phone.svg'
 export default function Home() {
-  const id = 1;
   return (
     <main>
       <Button />
-      <Icon iconSrc='phone-call' />
-      <img src="./assets/icons/phone.svg" width={50} height={50} alt="" />
-      <p>sad</p>
+      <Icon iconSrc='phone-call11'/>
     </main>
   );
 }
