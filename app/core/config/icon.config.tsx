55025086e73c0ee3eed26295c056ce7ad4d26dfc
Shay <shaybush93@gmail.com>
2023-07-23 00:35:11 +0300

    add icon

--- app/core/config/icon.config.tsx
+++ app/core/config/icon.config.tsx
@@ -1,5 +1,6 @@
-const configLink = "/icons"
+const configLink = "/icons";
 
 export const iconConfig: Record<string, string> = Object.freeze({
-    'phone-call' : `${configLink}/phone.svg`
+    'phone-call': `${configLink}/phone.svg`,
+    'check': `${configLink}/check.svg`
 });
