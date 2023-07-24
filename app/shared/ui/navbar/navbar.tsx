04714a1b17a6e8dedc48da566ca7c0c9dafaefdc
Shay Bushary <shay.bushary@1touch.io>
2023-07-24 12:39:50 +0300

    setup sidebar and navbar should add in jira task

---
+++ app/shared/ui/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface INavbarModel { }
+
+const Navbar: FC<INavbarModel> = ({ }) => {
+    return (
+        <div>Navbar</div>
+    );
+};
+
+export default Navbar;
---
+++ app/shared/ui/sidebar/sidebar.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface ISidebarModel { }
+
+const Sidebar: FC<ISidebarModel> = ({ }) => {
+    return (
+        <div>Sidebar</div>
+    );
+};
+
+export default Sidebar;
